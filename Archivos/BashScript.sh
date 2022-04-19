fbb80ccefe60bcfa350496b9f18de8bc5d566305
Jovanny Romo <79547958+jromoj@users.noreply.github.com>
2022-04-19 09:30:39 -0500

    Delete Archivos directory

--- Archivos/BashScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# chmod +x program_name (da permiso para poder ejecutarlo)
-# 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 (Github)
-# Escribir la secuencia de comandos 
-
-cd Prueba
-c++ a.cpp
-./a.out
